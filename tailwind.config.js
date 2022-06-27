b4f52cc97f449b0d1adf8e778447d3893a186d08
Bean791 <bean90917@gmail.com>
2022-06-27 10:33:22 +0700

    oke kerak jadi

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+      'satu':'#7C3E66',
+      'dua':'#F2EBE9',
+      'tiga':'#A5BECC',
+      'empat':'#243A73'
+      },
+      fontFamily:{
+        'font1':['Indie Flower'],
+        'font2':['Caveat']
+      }
+    },
+  },
+  plugins: [],
+}
